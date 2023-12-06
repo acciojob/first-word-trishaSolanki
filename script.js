74c7a252a3478c775978c0d846724f5bb7cc02ea
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-06 12:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+    if (s === '' || !s.includes(' ')) {
+        return s;
+    } else {
+        const spaceIndex = s.indexOf(' ');
+        return s.slice(0, spaceIndex);
+    }
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
